4c99d1e92ac80dc48a7756aa139f110f628387f9
Gregory Glasby <gglasby@unc.edu>
2025-03-29 14:29:18 -0400

    Finish MVP chrome-extension

--- extension/rollup.config.js
+++ extension/rollup.config.js
@@ -1,21 +1,74 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import polyfillNode from 'rollup-plugin-polyfill-node'
+import replace from '@rollup/plugin-replace';
+import dotenv from 'dotenv';
 
-export default {
-  input: 'popup.js',
-  output: {
-    file: 'dist/popup.js',
-    format: 'iife', 
-    name: 'bundle',
-    inlineDynamicImports: true
+export default [
+  {
+    input: 'popup.js',
+    output: {
+      file: 'dist/popup.js',
+      format: 'iife', 
+      name: 'bundle',
+      inlineDynamicImports: true
+    },
+    plugins: [
+      replace({
+        preventAssignment: true,
+        'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
+        'process.env.SUPABASE_KEY': JSON.stringify(process.env.SUPABASE_KEY),
+      }),
+      resolve({
+        browser: true,
+        preferBuiltins: false
+      }),
+      commonjs(),
+      polyfillNode()
+    ]
   },
-  plugins: [
-    resolve({
-      browser: true,
-      preferBuiltins: false
-    }),
-    commonjs(),
-    polyfillNode()
-  ]
-}
+  {
+    input: 'content.js',
+    output: {
+      file: 'dist/content.js',
+      format: 'iife',
+      name: 'contentBundle',
+      inlineDynamicImports: true
+    },
+    plugins: [
+      replace({
+        preventAssignment: true,
+        'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
+        'process.env.SUPABASE_KEY': JSON.stringify(process.env.SUPABASE_KEY),
+      }),
+      resolve({
+        browser: true,
+        preferBuiltins: false
+      }),
+      commonjs(),
+      polyfillNode()
+    ]
+  },
+  {
+    input: 'background.js',
+    output: {
+      file: 'dist/background.js',
+      format: 'iife',
+      name: 'contentBundle',
+      inlineDynamicImports: true
+    },
+    plugins: [
+      replace({
+        preventAssignment: true,
+        'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
+        'process.env.SUPABASE_KEY': JSON.stringify(process.env.SUPABASE_KEY),
+      }),
+      resolve({
+        browser: true,
+        preferBuiltins: false
+      }),
+      commonjs(),
+      polyfillNode()
+    ]
+  }
+]
